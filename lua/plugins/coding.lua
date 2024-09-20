6841c4e314cc25caf14497f2038506166b1202aa
blyedev <adam.smielewski@gmail.com>
2024-09-20 22:45:02 +0200

    Formatted files

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
-  {-- Provide default configurations to LSP's
+  { -- Provide default configurations to LSP's
     "neovim/nvim-lspconfig",
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
@@ -60,15 +60,15 @@ return {
         end
 
         -- Keymaps
-        map("gd",         require("telescope.builtin").lsp_definitions,               "[G]oto [D]efinition")
-        map("gr",         require("telescope.builtin").lsp_references,                "[G]oto [R]eferences")
-        map("gI",         require("telescope.builtin").lsp_implementations,           "[G]oto [I]mplementation")
-        map("<leader>D",  require("telescope.builtin").lsp_type_definitions,          "Type [D]efinition")
-        map("<leader>ds", require("telescope.builtin").lsp_document_symbols,          "[D]ocument [S]ymbols")
+        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+        map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+        map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
         map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-        map("<leader>rn", vim.lsp.buf.rename,                                         "[R]e[n]ame")
-        map("<leader>ca", vim.lsp.buf.code_action,                                    "[C]ode [A]ction", { "n", "x" })
-        map("gD",         vim.lsp.buf.declaration,                                    "[G]oto [D]eclaration")
+        map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
         -- Highligh keyword
         if client.supports_method("textDocument/documentHighlight") then
