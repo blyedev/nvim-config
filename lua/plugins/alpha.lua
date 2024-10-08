617d0db84bef7bfb82536eb8f3062d8c3f6fb559
blyedev <adam.smielewski@gmail.com>
2024-10-08 22:00:05 +0200

    Added a customized greeter

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,32 @@
+---@type LazySpec
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    dashboard.section.header.val = require("plugins.ascii.matrix").wake_up_multiline
+
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
+      dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<cr>"),
+      dashboard.button("g", "  Find text", "<cmd>Telescope live_grep<cr>"),
+      dashboard.button("c", "  Config", "<cmd>Telescope find_files cwd=" .. vim.fn.stdpath("config") .. "<cr>"),
+      dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<cr>"),
+      dashboard.button("q", "  Quit", "<cmd>qa<cr>"),
+    }
+
+    local handle = io.popen("fortune")
+    local fortune = handle:read("*a")
+    handle:close()
+    dashboard.section.footer.val = fortune
+
+    dashboard.config.opts.noautocmd = true
+
+    vim.cmd([[autocmd User AlphaReady echo 'ready']])
+
+    alpha.setup(dashboard.config)
+  end,
+}
