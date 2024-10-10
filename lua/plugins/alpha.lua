697e622beaad88cbb6212a13b3f0074dc4ffda40
blyedev <adam.smielewski@gmail.com>
2024-10-10 12:17:59 +0200

    Visual tweaks

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,14 +7,19 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-    dashboard.section.header.val = require("plugins.ascii.creation_of_adam").hands
+    dashboard.section.header.val = require("plugins.ascii.creation_of_adam").hands_no_bg
     -- dashboard.section.header.val = require("plugins.ascii.kingdom_of_heaven").king_hand
 
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
+      dashboard.button("N", "  New file", "<cmd>ene <BAR> startinsert<cr>"),
       dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<cr>"),
+      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
       dashboard.button("g", "  Find text", "<cmd>Telescope live_grep<cr>"),
-      dashboard.button("c", "  Config", "<cmd>Telescope find_files cwd=" .. vim.fn.stdpath("config") .. "<cr>"),
+      dashboard.button(
+        "n",
+        "  Neovim config",
+        "<cmd>Telescope find_files cwd=" .. vim.fn.stdpath("config") .. "<cr>"
+      ),
       dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<cr>"),
       dashboard.button("q", "  Quit", "<cmd>qa<cr>"),
     }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 ---@type LazySpec
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme tokyonight-night]])
-  end,
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   enabled = false,
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd([[colorscheme tokyonight-night]])
+  --   end,
+  -- },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme rose-pine-dawn]])
+    end,
+  },
 }
